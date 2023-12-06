ea03411ebf56b1f70c43a127e5814b40c305005b
Tien Dat <dattruong792001@gmail.com>
2023-12-06 16:51:51 +0700

    kiem loi form

--- dangky.php
+++ dangky.php
@@ -48,26 +48,31 @@ if (isset($_POST['dangky']) && ($_POST['dangky'])) {
                 <div class="form-group">
                     <label for="hovaten">Họ và tên: </label>
                     <input type="text" name="hovaten" id="hovaten" value="<?= $hovaten ?>">
+                    <p id="checkHovaten"></p>
                 </div>
 
                 <div class="form-group">
                     <label for="username">Tên đăng nhập: </label>
                     <input class="tendangky" type="text" name="username" id="username" value="<?= $username ?>">
+                    <p id="checkUsername"></p>
                 </div>
 
                 <div class="form-group">
                     <label class="form-group" for="password">Mật khẩu: <?= $checkmk ? '' : '<span style="color: red; font-size: 16px;">Mật khẩu không khớp!</span>' ?></label>
                     <input class="mkdangnhap" type="password" name="password" id="password">
+                    <p id="checkMk"></p>
                 </div>
 
                 <div class="form-group">
                     <label class="form-group" for="nhaplai_password">Nhập lại mật khẩu: </label>
-                    <input class="nhaplai_mkdangky" type="password" name="nhaplai_password" id="nhaplai-password">
+                    <input class="nhaplai_mkdangky" type="password" name="nhaplai_password" id="nhaplai_password">
+                    <p id="checkMknhaplai"></p>
                 </div>
 
                 <div class="form-group">
                     <label for="email">Email: </label>
                     <input type="email" name="email" id="email" value="<?= $email ?>">
+                    <p id="checkEmail"></p>
                 </div>
                 <?php
                 if (isset($thongbao)) {
@@ -75,14 +80,97 @@ if (isset($_POST['dangky']) && ($_POST['dangky'])) {
                 }
                 ?>
 
-                <input class="dangky" type="submit" name="dangky" value="Đăng ký">
-                
+                <input onclick="return checkForm()" class="dangky" type="submit" name="dangky" value="Đăng ký">
+
                 <div class="footer-form">
                     <p>Bạn đã có tài khoản. <a href="dangnhap.php">Đăng nhập ngay</a></p>
                 </div>
             </form>
         </div>
     </div>
+    <script>
+        function checkForm() {
+            var hovaten = document.getElementById("hovaten");
+            var username = document.getElementById("username");
+            var password = document.getElementById("password");
+            var nhaplai_password = document.getElementById("nhaplai_password");
+            var email = document.getElementById("email");
+
+            if (hovaten.value != "") {
+                if (hovaten.value.length < 4 || hovaten.value.length > 35) {
+                    document.getElementById("checkHovaten").innerText = "Bạn phải nhập từ 4-30 kí tự";
+                    document.getElementById("checkHovaten").style.color = "red";
+                    document.getElementById("hovaten").style.border = "1px red solid";
+                    hovaten.focus();
+                    return false;
+                } else {
+                    document.getElementById("checkHovaten").innerText = "";
+                    document.getElementById("hovaten").style.border = "2px green solid";
+                }
+            } else {
+                document.getElementById("checkHovaten").innerText = "Bạn không được để trống. Vui lòng nhập họ và tên.";
+                document.getElementById("checkHovaten").style.color = "red";
+                document.getElementById("hovaten").style.border = "1px red solid";
+                hovaten.focus();
+                return false;
+            }
+
+            if (username.value != "") {
+                if (username.value.length < 4) {
+                    document.getElementById("checkUsername").innerText = "Bạn phải nhập từ 4 ký tự";
+                    document.getElementById("checkUsername").style.color = "red";
+                    document.getElementById("username").style.border = "1px red solid";
+                    username.focus();
+                    return false;
+                } else {
+                    document.getElementById("checkUsername").innerText = "";
+                    document.getElementById("username").style.border = "2px green solid";
+                }
+            } else {
+                document.getElementById("checkUsername").innerText = "Bạn không được để trống. Vui lòng nhập tên đăng nhập";
+                document.getElementById("checkUsername").style.color = "red";
+                document.getElementById("username").style.border = "1px red solid";
+                username.focus();
+                return false;
+            }
+
+            if (password.value != "") {
+                if (password.value.length < 4) {
+                    document.getElementById("checkMk").innerText = "Bạn phải nhập từ 4 ký tự";
+                    document.getElementById("checkMk").style.color = "red";
+                    document.getElementById("password").style.border = "1px red solid";
+                    password.focus();
+                    return false;
+                } else {
+                    document.getElementById("checkMk").innerText = "";
+                    document.getElementById("password").style.border = "2px green solid";
+                }
+            } else {
+                document.getElementById("checkMk").innerText = "Bạn không được để trống. Vui lòng nhập mật khẩu.";
+                document.getElementById("checkMk").style.color = "red";
+                document.getElementById("password").style.border = "1px red solid";
+                password.focus();
+                return false;
+            }
+
+            if (nhaplai_password.value == "") {
+                document.getElementById("checkMknhaplai").innerText = "Bạn không được để trống.";
+                document.getElementById("checkMknhaplai").style.color = "red";
+                document.getElementById("nhaplai_password").style.border = "1px red solid";
+                nhaplai_password.focus();
+                return false;
+            }
+
+            if (email.value == "") {
+                document.getElementById("checkEmail").innerText = "Bạn không được để trống. Vui lòng nhập email.";
+                document.getElementById("checkEmail").style.color = "red";
+                document.getElementById("email").style.border = "1px red solid";
+                email.focus();
+                return false;
+            }
+            return true;
+        }
+    </script>
 
     <?php
     include './view/cuoitrang.php';
--- dangnhap.php
+++ dangnhap.php
@@ -1,23 +1,23 @@
 <?php
-    session_start();
-    include "./DAO/pdo.php";
-    include "./DAO/taikhoan.php";
+session_start();
+include "./DAO/pdo.php";
+include "./DAO/taikhoan.php";
 
-    if (isset($_POST['dangnhap']) && ($_POST['dangnhap'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $check_taikhoan = check_taikhoan($username, $password);
-        if(is_array($check_taikhoan)){
-            $_SESSION['user'] = $check_taikhoan;
-            header("location: index.php");
-        }
-        if($username || $password != $check_taikhoan){
-            $thongbao = "Tài khoản không tồn tại. Vui Lòng kiểm tra hoặc đăng ký!";
-        }
-        if($password!=$check_taikhoan){
-            $thongbao = "Mật khẩu sai.";
-        }
+if (isset($_POST['dangnhap']) && ($_POST['dangnhap'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $check_taikhoan = check_taikhoan($username, $password);
+    if (is_array($check_taikhoan)) {
+        $_SESSION['user'] = $check_taikhoan;
+        header("location: index.php");
     }
+    if ($username || $password != $check_taikhoan) {
+        $thongbao = "Tài khoản không tồn tại. Vui Lòng kiểm tra hoặc đăng ký!";
+    }
+    if ($password != $check_taikhoan) {
+        $thongbao = "Mật khẩu sai.";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -48,30 +48,78 @@
                 <div class="form-group">
                     <label for="username">Tên đăng nhập:</label>
                     <input class="tendangnhap" type="text" name="username" id="username">
+                    <p id="checkUsername"></p>
                 </div>
 
                 <div class="form-group">
                     <label class="form-group" for="password">Mật khẩu:</label>
                     <input class="mkdangnhap" type="password" name="password" id="password">
+                    <p id="checkMk"></p>
                 </div>
                 <?php
-                    if (isset($thongbao)) {
-                        echo '<h5 style="color:red">' . $thongbao . '</h5>';
-                    }
-                    ?>
+                if (isset($thongbao)) {
+                    echo '<h5 style="color:red">' . $thongbao . '</h5>';
+                }
+                ?>
 
-                <input class="dangnhap" type="submit" name="dangnhap" value="Đăng nhập">
+                <input onclick="return checkForm()" class="dangnhap" type="submit" name="dangnhap" value="Đăng nhập">
                 <div class="footer-form">
                     <a href="./quenmk.php">Quên mật khẩu?</a>
                     <p>Bạn chưa có tài khoản? <a href="dangky.php">Đăng ký ngay?</a></p>
                 </div>
-                <p class="quaylai">Trở về trang chủ: <a  href="./index.php">Tại đây</a></p>
+                <p class="quaylai">Trở về trang chủ: <a href="./index.php">Tại đây</a></p>
             </form>
         </div>
     </div>
+
+    <script>
+        function checkForm() {
+            var username = document.getElementById("username");
+            var password = document.getElementById("password");
+
+            if (username.value != "") {
+                if (username.value.length < 4) {
+                    document.getElementById("checkUsername").innerText = "Bạn phải nhập từ 4 ký tự";
+                    document.getElementById("checkUsername").style.color = "red";
+                    document.getElementById("username").style.border = "1px red solid";
+                    username.focus();
+                    return false;
+                } else {
+                    document.getElementById("checkUsername").innerText = "";
+                    document.getElementById("username").style.border = "2px green solid";
+                }
+            } else {
+                document.getElementById("checkUsername").innerText = "Bạn không được để trống. Vui lòng nhập tên đăng nhập";
+                document.getElementById("checkUsername").style.color = "red";
+                document.getElementById("username").style.border = "1px red solid";
+                username.focus();
+                return false;
+            }
+
+            if (password.value != "") {
+                if (password.value.length < 3) {
+                    document.getElementById("checkMk").innerText = "Bạn phải nhập từ 3 ký tự";
+                    document.getElementById("checkMk").style.color = "red";
+                    document.getElementById("password").style.border = "1px red solid";
+                    password.focus();
+                    return false;
+                } else {
+                    document.getElementById("checkMk").innerText = "";
+                    document.getElementById("password").style.border = "2px green solid";
+                }
+            } else {
+                document.getElementById("checkMk").innerText = "Bạn không được để trống. Vui lòng nhập mật khẩu.";
+                document.getElementById("checkMk").style.color = "red";
+                document.getElementById("password").style.border = "1px red solid";
+                password.focus();
+                return false;
+            }
+        }
+    </script>
 
     <?php
     include './view/cuoitrang.php';
     ?>
 </body>
+
 </html>
